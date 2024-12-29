779145fcf4ab29ca1ab4888c4bdb9ee49a787267
Jaeho Huh <toywogh@gmail.com>
2024-12-30 03:25:13 +0700

    d

--- models/User.js
+++ models/User.js
@@ -6,13 +6,7 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
 });
 
-// 비밀번호 해시 처리
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  const salt = await bcryptjs.genSalt(10);
-  this.password = await bcryptjs.hash(this.password, salt);
-  next();
-});
+
 
 // 비밀번호 비교 메서드
 userSchema.methods.comparePassword = async function (password) {
